5c4fff16f4a646d4f3c471c0140e1901ff829945
Kent Fredric <kentfredric@gmail.com>
2010-06-03 10:33:48 +1200

    Build of 4a49c3f0f, 0.01001713 on CPAN.

--- lib/Dist/Zilla/PluginBundle/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/KENTNL.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Dist::Zilla::PluginBundle::KENTNL;
 BEGIN {
-  $Dist::Zilla::PluginBundle::KENTNL::VERSION = '0.01001712';
+  $Dist::Zilla::PluginBundle::KENTNL::VERSION = '0.01001713';
 }
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
@@ -148,7 +148,7 @@ Dist::Zilla::PluginBundle::KENTNL - BeLike::KENTNL when you build your distribut
 
 =head1 VERSION
 
-version 0.01001712
+version 0.01001713
 
 =head1 DESCRIPTION
 
