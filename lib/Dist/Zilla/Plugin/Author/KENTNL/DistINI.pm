4aeb49b3d50425b76e2e39972bf970af56631dbe
Kent Fredric <kentfredric@gmail.com>
2010-07-24 08:53:53 +1200

    more critic happyness

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
-# ABSTRACT: Generate a dist.ini for @KENTNL projects.
+# ABSTRACT: Generate a C<dist.ini> for @KENTNL projects.
 
 use Moose;
 
@@ -47,16 +47,16 @@ relative frame of reference.
   Plugin::Author::KENTNL::* # Low pollution, well clustered.
 
 As it is, the following stuff is starting to get to me in terms of
-namespace pollution:
+name-space pollution:
 
   Plugin::EOLTests  # Would prefer Plugin::Test::EOL
   Plugin::PodSyntaxTests # Would prefer Plugin::Test::Pod::Syntax
 
-And I have half a mind to rename L<Dist::Zilla::PluginBundle::KENTNL|Dist::Zilla::PluginBundle::KENTNL> to be C<Dist::Zilla::PluginBundle::Author::KENTNL> just to keep the top level cleaner, for stuff where bundles of plugins are useful for people other than ... well.. me. Call me a counter-egotist, if you will.
+And I have half a mind to rename L<< C<Dist::Zilla::PluginBundle::KENTNL>|Dist::Zilla::PluginBundle::KENTNL >> to be C<Dist::Zilla::PluginBundle::Author::KENTNL> just to keep the top level cleaner, for stuff where bundles of plug-ins are useful for people other than ... well.. me. Call me a counter-egotist, if you will.
 
 =head1 THEFT
 
-This code is mostly stolen from the L<< C<DistINI>|Dist::Zilla::Plugin::DistINI >> plugin. Blame C<rjbs> if its broken =).
+This code is mostly stolen from the L<< C<DistINI>|Dist::Zilla::Plugin::DistINI >> plug-in. Blame C<rjbs> if its broken =).
 
 =cut
 
@@ -72,7 +72,7 @@ use String::Formatter named_stringf => {
 
 =method gather_files
 
-generates a dist.ini file.
+generates a C<dist.ini> file.
 
 =cut
 
