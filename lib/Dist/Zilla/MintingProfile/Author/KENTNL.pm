63c741608f8c92cdb7f5f3a865b15380b9b97fb9
Kent Fredric <kentfredric@gmail.com>
2011-01-02 08:31:12 +1300

    Critic

--- lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
+++ lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
@@ -4,9 +4,17 @@ use warnings;
 package Dist::Zilla::MintingProfile::Author::KENTNL;
 
 # ABSTRACT: KENTNL's Minting Profile
+
 use Moose;
 use namespace::autoclean;
 with 'Dist::Zilla::Role::MintingProfile::ShareDir';
 
+=head1 SYNOPSIS
+
+    dzil new -P Author::KENTNL Some::Dist::Name
+
+=cut
+
 __PACKAGE__->meta->make_immutable;
+no Moose;
 1;
