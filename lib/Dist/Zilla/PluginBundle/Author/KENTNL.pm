064c571d842556bdd76593a09df6d6426516ee4f
Kent Fredric <kentfredric@gmail.com>
2012-08-08 14:43:43 +1200

    Build results of 471a756 (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::PluginBundle::Author::KENTNL::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::PluginBundle::Author::KENTNL::VERSION = '1.4.3';
+  $Dist::Zilla::PluginBundle::Author::KENTNL::VERSION = '1.4.4';
 }
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
@@ -278,7 +278,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 1.4.3
+version 1.4.4
 
 =head1 SYNOPSIS
 
