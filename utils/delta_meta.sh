954d51e056cc7ae34d8a3becd82143d8ace871b1
Kent Fredric <kentfredric@gmail.com>
2013-04-27 06:56:40 +1200

    Build results of 9ff6a62 (on master)

---
+++ utils/delta_meta.sh
@@ -0,0 +1,12 @@
+
+self=$(realpath $0);
+selfdir=$(dirname $self);
+
+perl $selfdir/delta_meta.pl \
+    <( perl $selfdir/git_fastcat.pl $1 META.json) \
+    <( perl $selfdir/git_fastcat.pl $2 META.json);
+
+perl $selfdir/delta_deps.pl \
+    <( perl $selfdir/git_fastcat.pl $1 META.json) \
+    <( perl $selfdir/git_fastcat.pl $2 META.json);
+
