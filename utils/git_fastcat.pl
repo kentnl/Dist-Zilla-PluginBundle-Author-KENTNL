54dd11cae7b626673821458bc6e4359cc0e9329b
Kent Fredric <kentfredric@gmail.com>
2013-04-27 07:03:23 +1200

    tidy utils

--- utils/git_fastcat.pl
+++ utils/git_fastcat.pl
@@ -5,14 +5,14 @@ use warnings;
 use utf8;
 
 if ( @ARGV != 2 ) {
-    warn "git_fastcat.pl commitish path";
-    exit 255;
+  warn "git_fastcat.pl commitish path";
+  exit 255;
 }
 
 use Git::Wrapper;
 use Cwd qw(cwd);
 
-my ( $revision, $path ) = @ARGV; 
+my ( $revision, $path ) = @ARGV;
 
 my $git = Git::Wrapper->new(cwd);
 
@@ -20,11 +20,11 @@ my $sha1 = [ $git->rev_parse($revision) ]->[0];
 
 #*STDERR->print("\e[31m SHA1: $sha1 \e[0m\n");
 
-my $ls_r = [ $git->ls_tree($sha1, $path) ]->[0];
+my $ls_r = [ $git->ls_tree( $sha1, $path ) ]->[0];
 
 #*STDERR->print("\e[31m ls_r: $ls_r \e[0m\n");
 
-my ( $left , $right ) = $ls_r=~ /^([^\t]+)\t(.*$)/;
+my ( $left, $right ) = $ls_r =~ /^([^\t]+)\t(.*$)/;
 
 #*STDERR->print("\e[31m left: $left \e[0m\n");
 
@@ -32,7 +32,7 @@ my ( $flags, $type, $sha ) = split / /, $left;
 
 #*STDERR->print("\e[31m sha: $sha \e[0m\n");
 
-for my $line ( $git->cat_file('-p', $sha)) {
-    print "$line\n";
+for my $line ( $git->cat_file( '-p', $sha ) ) {
+  print "$line\n";
 }
 
