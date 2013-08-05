086aab37626b0078624f718160973756654c209f
Kent Fredric <kentfredric@gmail.com>
2013-08-05 19:23:40 +1200

    Build results of 6e7dda6 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::Plugin::Author::KENTNL::DistINI::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Plugin::Author::KENTNL::DistINI::VERSION = '1.7.5';
+  $Dist::Zilla::Plugin::Author::KENTNL::DistINI::VERSION = '1.7.6';
 }
 
 # ABSTRACT: Generate a C<dist.ini> for @Author::KENTNL projects.
@@ -122,7 +122,7 @@ Dist::Zilla::Plugin::Author::KENTNL::DistINI - Generate a C<dist.ini> for @Autho
 
 =head1 VERSION
 
-version 1.7.5
+version 1.7.6
 
 =head1 SYNOPSIS
 
--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::PluginBundle::Author::KENTNL::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::PluginBundle::Author::KENTNL::VERSION = '1.7.5';
+  $Dist::Zilla::PluginBundle::Author::KENTNL::VERSION = '1.7.6';
 }
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
@@ -291,7 +291,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 1.7.5
+version 1.7.6
 
 =head1 SYNOPSIS
 
