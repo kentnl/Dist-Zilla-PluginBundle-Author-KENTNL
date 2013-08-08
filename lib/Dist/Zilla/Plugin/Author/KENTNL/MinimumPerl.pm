64e341bba99104484c6970706684737012e2d49e
Kent Fredric <kentfredric@gmail.com>
2013-08-08 12:59:07 +1200

    Build results of 59080e7 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::VERSION = '1.7.7';
+  $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::VERSION = '1.8.0';
 }
 
 # FILENAME: MinimumPerl.pm
@@ -136,7 +136,7 @@ Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl - The MinimumPerl Plugin with a
 
 =head1 VERSION
 
-version 1.7.7
+version 1.8.0
 
 =head1 METHODS
 
