a4abfeddf9fb7a30b83ede5277c669f2c79a6b2a
Kent Fredric <kentfredric@gmail.com>
2013-11-18 16:38:26 +1300

    Build results of b1a0017 (on master)

--- lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
+++ lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::MintingProfile::Author::KENTNL::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::MintingProfile::Author::KENTNL::VERSION = '2.002000';
+  $Dist::Zilla::MintingProfile::Author::KENTNL::VERSION = '2.002001';
 }
 
 # ABSTRACT: KENTNL's Minting Profile
@@ -33,7 +33,7 @@ Dist::Zilla::MintingProfile::Author::KENTNL - KENTNL's Minting Profile
 
 =head1 VERSION
 
-version 2.002000
+version 2.002001
 
 =head1 SYNOPSIS
 
