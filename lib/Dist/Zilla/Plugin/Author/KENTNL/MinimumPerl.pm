d3b04632bd9cb34d6b0f92f8cfd14dbeab47b7c9
Kent Fredric <kentfredric@gmail.com>
2013-11-22 13:12:27 +1300

    Build results of b1a0017 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::VERSION = '2.002001';
+  $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::VERSION = '2.002002';
 }
 
 # FILENAME: MinimumPerl.pm
@@ -136,7 +136,7 @@ Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl - The MinimumPerl Plugin with a
 
 =head1 VERSION
 
-version 2.002001
+version 2.002002
 
 =head1 METHODS
 
