b5e5ff99a3f600dc72ebd2fb41c89a8467e5f93e
Kent Fredric <kentfredric@gmail.com>
2013-11-22 13:33:32 +1300

    Build results of 0b1416b (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::PluginBundle::Author::KENTNL::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::PluginBundle::Author::KENTNL::VERSION = '2.002001';
+  $Dist::Zilla::PluginBundle::Author::KENTNL::VERSION = '2.003000';
 }
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
@@ -279,7 +279,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 2.002001
+version 2.003000
 
 =head1 SYNOPSIS
 
