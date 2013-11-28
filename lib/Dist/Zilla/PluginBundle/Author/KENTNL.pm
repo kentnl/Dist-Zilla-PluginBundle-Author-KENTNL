5a2dc91c6910c8087ad75deef266d512315cf27f
Kent Fredric <kentfredric@gmail.com>
2013-11-29 02:29:07 +1300

    Build results of ca2793b (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::PluginBundle::Author::KENTNL::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::PluginBundle::Author::KENTNL::VERSION = '2.004001';
+  $Dist::Zilla::PluginBundle::Author::KENTNL::VERSION = '2.004002';
 }
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
@@ -279,7 +279,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 2.004001
+version 2.004002
 
 =head1 SYNOPSIS
 
