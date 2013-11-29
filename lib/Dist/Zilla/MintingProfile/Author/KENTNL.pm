c1501006c58d87632b3c3481623fb119a50af94d
Kent Fredric <kentfredric@gmail.com>
2013-11-30 08:16:43 +1300

    Build results of ca2793b (on master)

--- lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
+++ lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::MintingProfile::Author::KENTNL::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::MintingProfile::Author::KENTNL::VERSION = '2.004003';
+  $Dist::Zilla::MintingProfile::Author::KENTNL::VERSION = '2.005000';
 }
 
 # ABSTRACT: KENTNL's Minting Profile
@@ -33,7 +33,7 @@ Dist::Zilla::MintingProfile::Author::KENTNL - KENTNL's Minting Profile
 
 =head1 VERSION
 
-version 2.004003
+version 2.005000
 
 =head1 SYNOPSIS
 
