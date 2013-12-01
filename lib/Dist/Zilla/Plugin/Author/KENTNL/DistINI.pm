0792040a84a48357093ad2dce48c909773764f0d
Kent Fredric <kentfredric@gmail.com>
2013-12-01 18:09:39 +1300

    Build results of fbdea54 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::Plugin::Author::KENTNL::DistINI::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Plugin::Author::KENTNL::DistINI::VERSION = '2.6.0';
+  $Dist::Zilla::Plugin::Author::KENTNL::DistINI::VERSION = '2.006001';
 }
 
 # ABSTRACT: Generate a C<dist.ini> for @Author::KENTNL projects.
@@ -122,7 +122,7 @@ Dist::Zilla::Plugin::Author::KENTNL::DistINI - Generate a C<dist.ini> for @Autho
 
 =head1 VERSION
 
-version 2.6.0
+version 2.006001
 
 =head1 SYNOPSIS
 
