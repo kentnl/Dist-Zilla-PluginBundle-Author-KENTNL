f672da4c8b4b02c0feb248473bbdead59ee53f7a
Kent Fredric <kentfredric@gmail.com>
2013-12-01 18:05:24 +1300

    Build results of 35ca6f9 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::VERSION = '2.6.0';
+  $Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl::VERSION = '2.006001';
 }
 
 # FILENAME: MinimumPerl.pm
@@ -136,7 +136,7 @@ Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl - The MinimumPerl Plugin with a
 
 =head1 VERSION
 
-version 2.6.0
+version 2.006001
 
 =head1 METHODS
 
