7c3d6e86450404f12401e2bcebc18c7745c2f128
Kent Fredric <kentfredric@gmail.com>
2014-01-30 20:31:55 +1300

    Build results of e1cf6b0 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -8,7 +8,7 @@ use utf8;
 
 
 package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
-$Dist::Zilla::Plugin::Author::KENTNL::DistINI::VERSION = '2.011002';
+$Dist::Zilla::Plugin::Author::KENTNL::DistINI::VERSION = '2.012000';
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
@@ -194,7 +194,7 @@ Dist::Zilla::Plugin::Author::KENTNL::DistINI - Generate a dist.ini for @Author::
 
 =head1 VERSION
 
-version 2.011002
+version 2.012000
 
 =head1 SYNOPSIS
 
