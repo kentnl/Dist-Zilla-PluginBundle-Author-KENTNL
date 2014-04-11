aa882be36fab9f7f938929b620e57f56f4e37058
Kent Fredric <kentfredric@gmail.com>
2014-04-11 13:41:15 +1200

    Build results of 2512df9 (on master)

--- share/profiles/default/skel/maint/perlcritic.rc.gen.pl
+++ share/profiles/default/skel/maint/perlcritic.rc.gen.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
-# FILENAME: bundle_to_ini.pl
-# CREATED: 02/06/14 01:48:56 by Kent Fredric (kentnl) <kentfredric@gmail.com>
+## no critic (Modules::RequireVersionVar)
 # ABSTRACT: Write an INI file from a bundle
 
+use 5.008;    # utf8
 use strict;
 use warnings;
 use utf8;
@@ -10,13 +10,13 @@ use utf8;
 use Carp qw( croak carp );
 use Perl::Critic::ProfileCompiler::Util qw( create_bundle );
 
+## no critic (ErrorHandling::RequireUseOfExceptions)
 my $bundle = create_bundle('Example::Author::KENTNL');
 $bundle->configure;
 
-my @stopwords = ();
-for my $var (@stopwords) {
-    $bundle->add_or_append_policy_field(
-        'Documentation::PodSpelling' => ( 'stop_words' => $var ) );
+my @stopwords = (qw());
+for my $wordlist (@stopwords) {
+  $bundle->add_or_append_policy_field( 'Documentation::PodSpelling' => ( 'stop_words' => $wordlist ) );
 }
 
 #$bundle->add_or_append_policy_field(
@@ -28,19 +28,19 @@ for my $var (@stopwords) {
 my $inf = $bundle->actionlist->get_inflated;
 
 my $config = $inf->apply_config;
+
 {
-    open my $rcfile, '>', './perlcritic.rc' or croak 'Cant open perlcritic.rc';
-    $rcfile->print( $config->as_ini, "\n" );
-    close $rcfile or croak 'Something fubared closing perlcritic.rc';
+  open my $rcfile, '>', './perlcritic.rc' or croak 'Cant open perlcritic.rc';
+  $rcfile->print( $config->as_ini, "\n" );
+  close $rcfile or croak 'Something fubared closing perlcritic.rc';
 }
 my $deps = $inf->own_deps;
 {
-    open my $depsfile, '>', './perlcritic.deps'
-      or croak 'Cant open perlcritic.deps';
-    for my $key ( sort keys %{$deps} ) {
-        $depsfile->printf( "%s~%s\n", $key, $deps->{$key} );
-        *STDERR->printf( "%s => %s\n", $key, $deps->{$key} );
-    }
-    close $depsfile or carp 'Something fubared closing perlcritic.deps';
+  open my $depsfile, '>', './perlcritic.deps' or croak 'Cant open perlcritic.deps';
+  for my $key ( sort keys %{$deps} ) {
+    $depsfile->printf( "%s~%s\n", $key, $deps->{$key} );
+    *STDERR->printf( "%s => %s\n", $key, $deps->{$key} );
+  }
+  close $depsfile or carp 'Something fubared closing perlcritic.deps';
 }
 
