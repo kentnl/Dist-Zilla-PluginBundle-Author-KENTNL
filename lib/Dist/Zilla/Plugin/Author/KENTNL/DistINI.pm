460cf9b07da0956cdc92dfc76bc01acff67bfeab
Kent Fredric <kentfredric@gmail.com>
2014-04-12 21:46:46 +1200

    Build results of 8227c22 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -8,7 +8,7 @@ use utf8;
 
 
 package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
-$Dist::Zilla::Plugin::Author::KENTNL::DistINI::VERSION = '2.013000';
+$Dist::Zilla::Plugin::Author::KENTNL::DistINI::VERSION = '2.013001';
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
@@ -188,7 +188,7 @@ Dist::Zilla::Plugin::Author::KENTNL::DistINI - Generate a dist.ini for @Author::
 
 =head1 VERSION
 
-version 2.013000
+version 2.013001
 
 =head1 SYNOPSIS
 
