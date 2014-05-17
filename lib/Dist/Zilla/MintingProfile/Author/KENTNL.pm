ca4f6bf9791c6639e8df90ea9ba926de21287d4f
Kent Fredric <kentfredric@gmail.com>
2014-05-18 01:45:38 +1200

    Build results of 1968ab7 (on master)

--- lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
+++ lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
@@ -4,7 +4,7 @@ use warnings;
 use utf8;
 
 package Dist::Zilla::MintingProfile::Author::KENTNL;
-$Dist::Zilla::MintingProfile::Author::KENTNL::VERSION = '2.013003';
+$Dist::Zilla::MintingProfile::Author::KENTNL::VERSION = '2.013004';
 # ABSTRACT: KENTNL's Minting Profile
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
@@ -50,7 +50,7 @@ Dist::Zilla::MintingProfile::Author::KENTNL - KENTNL's Minting Profile
 
 =head1 VERSION
 
-version 2.013003
+version 2.013004
 
 =head1 SYNOPSIS
 
