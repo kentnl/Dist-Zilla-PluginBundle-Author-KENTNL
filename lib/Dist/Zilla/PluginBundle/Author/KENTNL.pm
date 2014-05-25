eadef796f6524dc84b1c1fd5f55aa17458465df3
Kent Fredric <kentfredric@gmail.com>
2014-05-26 07:31:59 +1200

    Build results of 82ae5d0 (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.013005';
+our $VERSION = '2.014000';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -590,7 +590,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 2.013005
+version 2.014000
 
 =head1 SYNOPSIS
 
