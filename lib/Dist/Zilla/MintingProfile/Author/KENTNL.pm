39545d8b81dd6d42cb1202f4a1cfdd6dbdcd1c59
Kent Fredric <kentfredric@gmail.com>
2014-06-06 21:11:44 +1200

    v2.016000
     [00 Major]
     - add copyfiles attribute to easily support copying files from build.
     - add srcreadme attribute to control how README is generated in the build tree.
     - Changes.deps* generator now writes files in misc/ to keep top level clutter down.
    
     [Dependencies::Stats]
     - Dependencies changed since 2.015000, see misc/*.deps* for details
     - develop: +1
     - runtime: +1 ↑2
     - test: ↑2

--- lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
+++ lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::MintingProfile::Author::KENTNL;
 
 # ABSTRACT: KENTNL's Minting Profile
 
-our $VERSION = '2.015001';
+our $VERSION = '2.016001';
 
 # AUTHORITY
 
--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.016000';
+our $VERSION = '2.016001';
 
 # AUTHORITY
 
