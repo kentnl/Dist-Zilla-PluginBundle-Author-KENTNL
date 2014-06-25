b3cc0a20858649766321184e2068077d7107e233
Kent Fredric <kentfredric@gmail.com>
2014-06-26 07:23:37 +1200

    v2.016001
     [00 Minor]
     - Rework dep_changes util to use CPAN::Meta::Prereqs::Diff
     - generated perlcritic generator uses misc/ now instead.
     - generated modules come with 5.008, utf8, AUTHORITY and bumpversion ready
     - generated dist.ini comes with bumpversions enabled
     - generated dist.ini comes with copylicense
    
     [Dependencies::Stats]
     - Dependencies changed since 2.016000, see misc/*.deps* for details
     - develop: ↑6
     - runtime: ↑9 (recommends: +4 -1)
     - test: ↑1

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -11,7 +11,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
-our $VERSION = '2.016001';
+our $VERSION = '2.016002';
 
 # AUTHORITY
 
