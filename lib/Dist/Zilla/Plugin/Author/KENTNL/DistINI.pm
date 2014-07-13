a8749a4d1467b4234e338690310ac978c35bc480
Kent Fredric <kentfredric@gmail.com>
2014-07-13 18:55:03 +1200

    v2.016002
     [00 Minor]
     - dep_changes util adds preludes describing the files.
    
     [Dependencies::Stats]
     - Dependencies changed since 2.016001, see misc/*.deps* for details
     - configure: ↑1
     - develop: (suggests: ↑1)
     - runtime: ↑4
     - test: ↑2

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -11,7 +11,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
-our $VERSION = '2.016002';
+our $VERSION = '2.016003';
 
 # AUTHORITY
 
