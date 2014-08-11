4a6dbba324a1a39039428d8543fa1721950ccf46
Kent Fredric <kentfredric@gmail.com>
2014-08-12 08:51:13 +1200

    Build results of aee3600 (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.017000';
+our $VERSION = '2.017001';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -760,7 +760,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 2.017000
+version 2.017001
 
 =head1 SYNOPSIS
 
