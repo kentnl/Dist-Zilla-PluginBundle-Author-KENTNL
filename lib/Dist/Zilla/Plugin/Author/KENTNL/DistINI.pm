ea09db89824186bcec1ac497aa4a190682d86055
Kent Fredric <kentfredric@gmail.com>
2014-08-12 08:41:05 +1200

    Build results of 5e97391 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -11,7 +11,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
-our $VERSION = '2.016009';
+our $VERSION = '2.017000';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -194,7 +194,7 @@ Dist::Zilla::Plugin::Author::KENTNL::DistINI - Generate a dist.ini for @Author::
 
 =head1 VERSION
 
-version 2.016009
+version 2.017000
 
 =head1 SYNOPSIS
 
