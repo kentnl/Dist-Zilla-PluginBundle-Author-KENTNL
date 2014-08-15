8690d68c91cfd0ad177caa00795303cbec2af789
Kent Fredric <kentfredric@gmail.com>
2014-08-15 14:12:25 +1200

    v2.017001
     [00 Bugfix]
     - Use a version of Git::Contributors that is safe for testing and general use.
    
     [Dependencies::Stats]
     - Dependencies changed since 2.017000, see misc/*.deps* for details
     - develop: ↑3
     - runtime: ↑6
     - test: ↑1

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.017001';
+our $VERSION = '2.017002';
 
 # AUTHORITY
 
