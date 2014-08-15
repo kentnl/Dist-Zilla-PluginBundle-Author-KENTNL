160faead1162a1b2899476536e4b78aeec36b4ce
Kent Fredric <kentfredric@gmail.com>
2014-08-16 09:05:16 +1200

    v2.017002
     [00 Trivial]
     - util updates.
     - whitespace adjustment
    
     [Misc]
     - Improve source side POD to reduce build side whitespace.
    
     [Utils]
     - dep_changes: Greatly increase the cache timing to 5h instead of 30m now it proves to be more stable.

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.017002';
+our $VERSION = '2.017003';
 
 # AUTHORITY
 
