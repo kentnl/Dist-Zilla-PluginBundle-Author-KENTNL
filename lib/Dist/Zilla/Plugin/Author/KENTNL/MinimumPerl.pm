029549095d797739a52f2d2644c8338ce71be5e1
Kent Fredric <kentfredric@gmail.com>
2014-09-06 14:46:30 +1200

    Build results of 3c8e558 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
@@ -24,8 +24,11 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
 
 use Moose qw( has extends override );
+use Dist::Zilla::Util::ConfigDumper qw( config_dumper );
 extends 'Dist::Zilla::Plugin::MinimumPerl';
 use namespace::autoclean;
+
+around dump_config => config_dumper( __PACKAGE__, { attrs => 'detected_perl', 'fiveten' } );
 
 has 'detected_perl' => (
   is         => 'rw',
