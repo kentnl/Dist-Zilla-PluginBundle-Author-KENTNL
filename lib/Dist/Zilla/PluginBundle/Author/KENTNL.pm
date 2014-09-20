a2de08b9017f1d30c32258d8e586cebdd41e9832
Kent Fredric <kentfredric@gmail.com>
2014-09-21 09:19:23 +1200

    v2.020002
     [Dependencies::Stats]
     - Dependencies changed since 2.020001, see misc/*.deps* for details
     - develop: ↑8
     - runtime: ↑10
     - test: ↑2
    
     [Utils]
     - dep_changes: Enable LMDB Backend as an option.
     - dep_changes: Cleanup CHI code to be more efficient and understandable.
     - dep_changes: Be more graceful about 'releases' branch being missing.

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.020002';
+our $VERSION = '2.020003';
 
 # AUTHORITY
 
