2333e3658ebb5ae20bb8a1e238fc442e90b62493
Kent Fredric <kentfredric@gmail.com>
2014-10-03 19:43:33 +1300

    v2.020004
     [Dependencies::Stats]
     - Dependencies changed since 2.020003, see misc/*.deps* for details
     - develop: ↑4
     - runtime: ↑8 (recommends: +2 -2)
     - test: ↑1 (recommends: ↑1)
    
     [Utils]
     - dep_changes.pl : Use LMDB driver by default
     - dep_changes.pl : Add several more levels of advanced caching for blazing fast performance.

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -11,7 +11,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
-our $VERSION = '2.020004';
+our $VERSION = '2.020005';
 
 # AUTHORITY
 
