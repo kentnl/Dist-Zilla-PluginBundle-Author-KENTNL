7646a00c48c7f35f3aa639b715a4039d8175cac2
Kent Fredric <kentfredric@gmail.com>
2014-10-05 23:37:43 +1300

    Build results of c71de1c (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -460,7 +460,7 @@ sub _configure_bundle_develop_requires {
   $self->add_named_plugin(
     'PluginAutoDeps' => 'Prereqs::Plugins' => {
       ':version' => '1.002000',
-      phase => 'develop',
+      phase      => 'develop',
     },
   );
   return if _is_bake();
