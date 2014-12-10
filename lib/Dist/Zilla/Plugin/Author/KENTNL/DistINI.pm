41f47b84df2bdb1398465281ccdd10ff96d281a6
Kent Fredric <kentfredric@gmail.com>
2014-12-10 23:00:38 +1300

    Build results of 3012e67 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -11,7 +11,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
-our $VERSION = '2.022001';
+our $VERSION = '2.022002';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -141,7 +141,7 @@ Dist::Zilla::Plugin::Author::KENTNL::DistINI - Generate a dist.ini for @Author::
 
 =head1 VERSION
 
-version 2.022001
+version 2.022002
 
 =head1 SYNOPSIS
 
--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.022001';
+our $VERSION = '2.022002';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -680,7 +680,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 2.022001
+version 2.022002
 
 =head1 SYNOPSIS
 
