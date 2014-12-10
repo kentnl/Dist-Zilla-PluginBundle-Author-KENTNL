3012e6735fb597afadad7018d0d12d2ad62d5c17
Kent Fredric <kentfredric@gmail.com>
2014-12-10 23:00:22 +1300

    v2.022001
     [00 Minor]
     - Drop ReadmeFromPod until its at least usable again.
     - Drop utf8 and 5.008 from the default module template.
    
     [Bugfix]
     - Make miniting profile tests happy by giving a valid abstract by default
     - Anticipate needing 5.006 as a prereq.
    
     [Dependencies::Stats]
     - Dependencies changed since 2.022000, see misc/*.deps* for details
     - configure: +1 (recommends: ↑1)
     - develop: ↑29 -1 (recommends: ↑1, suggests: ↑1)
     - runtime: ↑37 -1
     - test: +1 ↑5 (recommends: ↑3)

--- lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
+++ lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::MintingProfile::Author::KENTNL;
 
 # ABSTRACT: KENTNL's Minting Profile
 
-our $VERSION = '2.022001';
+our $VERSION = '2.022002';
 
 # AUTHORITY
 
--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.022001';
+our $VERSION = '2.022002';
 
 # AUTHORITY
 
