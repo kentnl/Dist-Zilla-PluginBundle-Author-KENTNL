16a38c354b1ba9e335774c570966413cf6c2e90f
Kent Fredric <kentfredric@gmail.com>
2014-12-30 00:29:17 +1300

    Build results of 17fa8c8 (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -538,9 +538,7 @@ sub _configure_readmes {
   my ($self) = @_;
 
   # TODO: Go back to the otherone when it stops failing balls
-  $self->add_named_plugin(
-    'ShippedReadme' => 'Pod2Readme' => {},
-  );
+  $self->add_named_plugin( 'ShippedReadme' => 'Pod2Readme' => {}, );
 
   my $type = $self->srcreadme;
 
