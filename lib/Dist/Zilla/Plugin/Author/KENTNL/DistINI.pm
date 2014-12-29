e4889cc5682f6c0a88b3f46e39f949ea76c49a06
Kent Fredric <kentfredric@gmail.com>
2014-12-30 00:44:26 +1300

    v2.022003
     [Dependencies::Stats]
     - Dependencies changed since 2.022002, see misc/*.deps* for details
     - develop: +1 ↑22 (recommends: ↑1)
     - runtime: +1 ↑26
     - test: ↑1 (recommends: ↑1)
    
     [Minor]
     - Switch to Pod2Readme for shipped readme because I much prefer how it renders

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -11,7 +11,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
-our $VERSION = '2.022003';
+our $VERSION = '2.022004';
 
 # AUTHORITY
 
