50830eb51352386f71d20176dbf7f3ec9295b500
Kent Fredric <kentfredric@gmail.com>
2015-01-02 16:02:12 +1300

    v2.022004
     [Dependencies::Stats]
     - Dependencies changed since 2.022003, see misc/*.deps* for details
     - develop: +1 ↑1
     - runtime: ↑4
     - test: +1
    
     [Minor]
     - use new travis generator
     - inject new travis generator into new dists
     - inject vim syntax stanza into new dists

--- lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl;
 
 # ABSTRACT: The MinimumPerl Plugin with a few hacks
 
-our $VERSION = '2.022004';
+our $VERSION = '2.022005';
 
 # AUTHORITY
 
--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.022004';
+our $VERSION = '2.022005';
 
 # AUTHORITY
 
