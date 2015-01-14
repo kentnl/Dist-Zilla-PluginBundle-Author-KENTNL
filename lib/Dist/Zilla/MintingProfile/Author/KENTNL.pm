f3549990ef19523c657ad70ecfc79e63aa257f84
Kent Fredric <kentfredric@gmail.com>
2015-01-14 20:28:36 +1300

    Build results of b29ad38 (on master)

--- lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
+++ lib/Dist/Zilla/MintingProfile/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::MintingProfile::Author::KENTNL;
 
 # ABSTRACT: KENTNL's Minting Profile
 
-our $VERSION = '2.023002';
+our $VERSION = '2.023003';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -46,7 +46,7 @@ Dist::Zilla::MintingProfile::Author::KENTNL - KENTNL's Minting Profile
 
 =head1 VERSION
 
-version 2.023002
+version 2.023003
 
 =head1 SYNOPSIS
 
