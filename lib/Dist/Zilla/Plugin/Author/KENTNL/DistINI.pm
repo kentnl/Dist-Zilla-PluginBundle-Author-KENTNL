7f10667df535a9598029fb1eb2b6962df3e0f380
Kent Fredric <kentfredric@gmail.com>
2015-02-06 00:10:57 +1300

    Build results of 8157220 (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -11,7 +11,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
-our $VERSION = '2.023002';
+our $VERSION = '2.023003';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -140,7 +140,7 @@ Dist::Zilla::Plugin::Author::KENTNL::DistINI - Generate a dist.ini for @Author::
 
 =head1 VERSION
 
-version 2.023002
+version 2.023003
 
 =head1 SYNOPSIS
 
--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.023002';
+our $VERSION = '2.023003';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -708,7 +708,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 2.023002
+version 2.023003
 
 =head1 SYNOPSIS
 
