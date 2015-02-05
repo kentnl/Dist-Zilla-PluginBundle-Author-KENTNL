8157220a33d6ccf0db164b9457192f36ac154ea4
Kent Fredric <kentfredric@gmail.com>
2015-02-06 00:10:53 +1300

    v2.023003
     [Bugfix]
     - Ensure quoted YAML via Travis stack upgrades.
    
     [Dependencies::Stats]
     - Dependencies changed since 2.023002, see misc/*.deps* for details
     - develop: ↑1 (suggests: ↑1)
     - runtime: (recommends: +1)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/DistINI.pm
@@ -11,7 +11,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::DistINI;
 
 # ABSTRACT: Generate a dist.ini for @Author::KENTNL projects.
 
-our $VERSION = '2.023003';
+our $VERSION = '2.023004';
 
 # AUTHORITY
 
--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.023003';
+our $VERSION = '2.023004';
 
 # AUTHORITY
 
