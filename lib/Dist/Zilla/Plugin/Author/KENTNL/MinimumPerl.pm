2d5bbb3e1796c27440cbd61449606e73b1e19fc8
Kent Fredric <kentfredric@gmail.com>
2015-03-01 02:12:16 +1300

    Build results of 101129b (on master)

--- lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/MinimumPerl.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl;
 
 # ABSTRACT: The MinimumPerl Plugin with a few hacks
 
-our $VERSION = '2.023004';
+our $VERSION = '2.025000';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -152,7 +152,7 @@ Dist::Zilla::Plugin::Author::KENTNL::MinimumPerl - The MinimumPerl Plugin with a
 
 =head1 VERSION
 
-version 2.023004
+version 2.025000
 
 =head1 METHODS
 
