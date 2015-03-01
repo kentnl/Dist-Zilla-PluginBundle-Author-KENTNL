5f298117935df8556bc24c8c3601638de78486e5
Kent Fredric <kentfredric@gmail.com>
2015-03-01 19:32:30 +1300

    Build results of ffe5dea (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.024001';
+our $VERSION = '2.024000';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -708,7 +708,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 2.024001
+version 2.024000
 
 =head1 SYNOPSIS
 
