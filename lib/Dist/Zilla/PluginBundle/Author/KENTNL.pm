4374cfe82ead4edf173e1d6b780a509368b0287a
Kent Fredric <kentfredric@gmail.com>
2015-07-05 15:53:55 +1200

    Build results of ece773f (on master)

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL.pm
@@ -7,7 +7,7 @@ package Dist::Zilla::PluginBundle::Author::KENTNL;
 
 # ABSTRACT: BeLike::KENTNL when you build your distributions.
 
-our $VERSION = '2.025001';
+our $VERSION = '2.025002';
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -738,7 +738,7 @@ Dist::Zilla::PluginBundle::Author::KENTNL - BeLike::KENTNL when you build your d
 
 =head1 VERSION
 
-version 2.025001
+version 2.025002
 
 =head1 SYNOPSIS
 
