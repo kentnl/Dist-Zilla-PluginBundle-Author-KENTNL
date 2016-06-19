19d51b609ad1ce3500df26bb4ecc1e090d3655e1
Kent Fredric <kentfredric@gmail.com>
2016-06-19 17:57:02 +1200

    Force upgrading to latest cpanm

--- maint/travisci.pl
+++ maint/travisci.pl
@@ -3,6 +3,7 @@
 sub {
   my ($yaml) = @_;
   splice @{ $yaml->{before_install} }, 1, 0, ('git --version');
+  splice @{ $yaml->{before_install} }, 1, 0, ('perlbrew install-cpanm -f');
 
   #  @{ $yaml->{matrix}->{include} } = grep { $_->{perl} ne '5.8' }  @{ $yaml->{matrix}->{include} };
 };
